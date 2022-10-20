29ce6fc15e177d3ba2b3b702fb8fa16ca952a213
Ilya <kanaplyasm@gmail.com>
2022-10-20 09:47:31 +0300

    смена раскладки в названии.

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TestApp.urls'
+ROOT_URLCONF = 'testapp.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TestApp.wsgi.application'
+WSGI_APPLICATION = 'testapp.wsgi.application'
 
 
 # Database
@@ -123,7 +124,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
